c93a8c9ea1ee50277aad006d17cc79e9743c89f8
randgalt <jordan@jordanzimmerman.com>
2017-11-28 12:36:16 -0800

    Added all the nicknames

--- src/test/scala/no/found/scala/jersey/ApiResource.scala
+++ src/test/scala/no/found/scala/jersey/ApiResource.scala
@@ -12,21 +12,30 @@ class ApiResource extends Api.TopLevel {
 
   val opGetModel = Api.Get(Description(
     description = "test",
-    nickname = "this-is-a-test",
+    nickname = "this-is-a-test-get",
     path = "{id}"
   )) { params =>
     Future.successful(Model("me", 42, params.segment("id")))
   }
 
-  val opPutModel = Api.Put(Description(description = "test")) { params: Entity[Model] =>
+  val opPutModel = Api.Put(Description(
+    description = "test",
+    nickname = "this-is-a-test-put",
+  )) { params: Entity[Model] =>
     Future.successful(Api.StatusResponse(Status.ACCEPTED.getStatusCode))
   }
 
-  val opPostModel = Api.Post(Description(description = "test")) { params: Entity[Model] =>
+  val opPostModel = Api.Post(Description(
+    description = "test",
+    nickname = "this-is-a-test-post",
+  )) { params: Entity[Model] =>
     Future.successful(params.entity().map(_.name).getOrElse("dunno"))
   }
 
-  val opDeleteModel = Api.Delete(Description(description = "test")) { params =>
+  val opDeleteModel = Api.Delete(Description(
+    description = "test",
+    nickname = "this-is-a-test-delete",
+  )) { params =>
     Future.successful(Api.StatusResponse(Status.ACCEPTED.getStatusCode))
   }
 }
